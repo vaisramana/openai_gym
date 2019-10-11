230c36e8c264ab0ef47493c0099f37f3b9e6e59d
shuo <zhangshuo.chn@gmail.com>
2019-10-11 14:18:05 +0800

    stop recording

--- agent.py
+++ agent.py
@@ -7,18 +7,18 @@ class random:
     def __init__(self, action_space):
         self.action_space = action_space
 
-    def act(self, observation, reward, done):
+    def act(self, observation):
         return self.action_space.sample()
 
     def learn(self, *args):
         pass
 
 
-class bespoke:
+class mountaincar_bespoke:
     def __init__(self, env):
         pass
     
-    def act(self, observation, reward, done):
+    def act(self, observation):
         position, velocity = observation
         lb = min(-0.09 * (position + 0.25) ** 2 + 0.03,
                 0.3 * (position + 0.9) ** 4 - 0.008)
--- run_env.py
+++ run_env.py
@@ -18,6 +18,30 @@ def make_env(env_name):
 
 
 
+def episode(env, agent, render=False, train=False):
+    episode_reward = 0. 
+    reward = 0
+    observation = env.reset() 
+    step = 0
+    while True: 
+        if render: 
+            env.render() 
+            sys.stdout.flush()
+        action = agent.act(observation)
+        next_observation, reward, done, _ = env.step(action) 
+        #print("%d ob %r rew %r done %r -> act %r -> ob %r rew %r done %r" %(step, observation, reward, done, action, next_observation, reward, done))
+        episode_reward += reward
+        if train:
+            agent.learn(observation, action, reward, done) 
+        if done:
+            #print("episode done with episode reward %d" %(episode_reward));
+            break
+        observation = next_observation
+        step += 1
+    return episode_reward
+
+
+
 
 
 if __name__ == '__main__':
@@ -31,38 +55,18 @@ if __name__ == '__main__':
 
     env = make_env(args.env_id)
 
-    # You provide the directory to write to (can be an existing
-    # directory, including one with existing data -- all monitor files
-    # will be namespaced). You can also dump to a tempdir if you'd
-    # like: tempfile.mkdtemp().
-    outdir = './random-agent-results'
-    env = wrappers.Monitor(env, directory=outdir, force=True)
     env.seed(0)
-    agent = agent.random(env.action_space)
-    #agent = agent.bespoke(env.action_space)
+    #agent = agent.random(env.action_space)
+    agent = agent.mountaincar_bespoke(env.action_space)
 
-    episode_count = 10
+    episode_count = 3
     reward = 0
     done = False
 
     for i in range(episode_count):
         ob = env.reset()
         step = 0
-        episode_reward = 0
-        while True:
-            action = agent.act(ob, reward, done)
-            next_ob, reward, done, _ = env.step(action)
-            print("%d/%d ob %r rew %r done %r -> act %r -> ob %r rew %r done %r" %(step, i, ob, reward, done, action, next_ob, reward, done))
-            step += 1
-            episode_reward += reward
-            agent.learn(ob, action, reward, done)
-            ob = next_ob
-            if done:
-                print("episode %d done with episode reward %d" %(i, episode_reward));
-                break
-            # Note there's no env.render() here. But the environment still can open window and
-            # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
-            # Video is not recorded every episode, see capped_cubic_video_schedule for details.
+        episode_reward = episode(env, agent, render=True, train=True)
 
     # Close the env and write monitor result info to disk
     env.close()
